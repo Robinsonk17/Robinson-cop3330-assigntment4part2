4df4eac7a8b34c8d3a2b375d6cfa47f241d8f5e5
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-11 23:53:43 -0400

    Create Item.java

---
+++ robinson-cop3330-assignment4part2/src/ucf/assignments/Item.java
@@ -0,0 +1,47 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
+
+package ucf.assignments;
+
+public class Item {
+    /*
+
+    boolean status
+    String description
+    String date
+
+    Constructor(String description, String date) {
+        this.description = description
+        this.date = date
+        this.status = false, new item created would not be completed
+    }
+
+    Methods -
+    setStatus(boolean status) {
+        this.status = status
+    }
+
+    getStatus() {
+        return status
+    }
+
+    setDescription(String description) {
+        this.description = description
+    }
+
+    getDescription() {
+        return description
+    }
+
+    setDate(String date) {
+        this.date = date
+    }
+
+    getDate() {
+        return date
+    }
+
+     */
+}
