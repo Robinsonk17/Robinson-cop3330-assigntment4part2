c66b7d408d91aa5d7a4dc288406722ecd18b8d98
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-11 23:53:45 -0400

    Create Controller.java

---
+++ robinson-cop3330-assignment4part2/src/ucf/assignments/Controller.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
+
+package ucf.assignments;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+
+public class Controller extends VBox {
+
+    public CheckBox choice1;
+    public CheckBox choice2;
+    public Button deleteButton;
+    public Button editButton;
+    public Button addButton;
+    public Button addList;
+    public Button closeList;
+    public Button deleteList;
+
+
+}
