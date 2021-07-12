0938f0028a02601a4782f43892229e9915fb1f17
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-11 23:54:00 -0400

    Create Main.java

---
+++ robinson-cop3330-assignment4part2/src/ucf/assignments/Main.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
+
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("todo-gui.fxml"));
+        primaryStage.setScene(new Scene(root, 640, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
