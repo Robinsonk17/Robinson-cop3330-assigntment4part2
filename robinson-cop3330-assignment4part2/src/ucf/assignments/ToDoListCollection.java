2c728dc408e0339cbbcbc83c5952f0596609055e
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-11 23:54:03 -0400

    Create ToDoListCollection.java

---
+++ robinson-cop3330-assignment4part2/src/ucf/assignments/ToDoListCollection.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
+
+package ucf.assignments;
+
+public class ToDoListCollection {
+    /*
+
+    List<ToDoList> lists
+
+    Constructor(empty) {
+        lists = new List, no-arg constructor because list is being created when program starts
+    }
+
+    Utility Methods -
+    loadList(FILE file) {
+        - the file passed in will be formatted in a way where the program will be able to parse its contents
+        - from the file:
+        -- read in amount of lists
+        -- parse item, list, add
+    }
+
+    saveList() {
+        - create file
+        - when file is created:
+        -- write contents to the file in the following order: quantity, item, list
+    }
+
+    removeList(ToDoList list) {
+        lists.remove(list)
+    }
+
+     */
+
+}
