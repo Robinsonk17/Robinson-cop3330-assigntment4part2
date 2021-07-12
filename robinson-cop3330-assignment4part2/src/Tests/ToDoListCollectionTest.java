89a20f55a3e3e493fbcbc6d865d4df7f5350ed01
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-11 23:53:36 -0400

    Create ToDoListCollectionTest.java

---
+++ robinson-cop3330-assignment4part2/src/Tests/ToDoListCollectionTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoListCollectionTest {
+    /*
+
+    loadListTest {
+        - Verify the file passed in will be formatted
+        -- read in amount of lists
+    }
+
+    saveListTest {
+        -- Verify files save when created
+        -- Verify quantity order saves when listed
+    }
+*/
+}
